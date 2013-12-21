2e59c7431e9f44d03af868eef9d113049d213073
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2013-12-21 04:29:51 +0100

    New version

--- package.js
+++ package.js
@@ -1,27 +1,21 @@
 Package.describe({
-  summary: "Model layer designed for Meteor."
+  summary: "Model layer for Meteor"
 });
 
 Package.on_use(function (api) {
-  api.use(['meteor', 'underscore', 'zeitgeist-class']);
+  api.use(['meteor', 'underscore']);
   
   api.add_files([
-    'lib/definition.js',
-    'lib/model_utils.js',
+    'lib/schema.js',
+    'lib/utils.js',
     'lib/model_base.js',
     'lib/model.js',
-    'lib/behavior_base.js',
-    'lib/behavior.js',
-    'lib/validator.js',
-    'lib/validators/string.js'
+    'lib/behavior.js'
     ], ['client', 'server']);
   
   api.export([
     'Model',
     'Behavior',
-    'Behaviors',
-    'Validator',
-    'Validators',
-    'StringValidator'
+    'Behaviors'
   ], ['client', 'server']);
 });
