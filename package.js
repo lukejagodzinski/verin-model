42bded00fdd29d98da88091780fff3310a7200f5
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2014-03-21 23:36:50 +0100

    Upgrade for the Meteor 0.7.2

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.use(['meteor', 'underscore']);
+  api.use(['meteor', 'underscore', 'minimongo']);
   
   api.add_files([
     'lib/schema.js',
